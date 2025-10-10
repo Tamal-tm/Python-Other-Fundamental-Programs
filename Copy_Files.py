73a8e77698cec4aee72ad3723db4d7f9c005c89d
Tamal Mallick <tamalmallick23@gmail.com>
2025-10-10 08:55:24 +0530

    Add files via upload

---
+++ Copy_Files.py
@@ -0,0 +1,4 @@
+from shutil import copyfile
+copyfile("C:/Users/catas/OneDrive/Desktop/VS Code Progs/Website Link.txt", "C:/Users/catas/OneDrive/Desktop/VS Code Progs/Tamal.txt")
+
+# File contents copied from source to destination. From Website link to Tamal. 
