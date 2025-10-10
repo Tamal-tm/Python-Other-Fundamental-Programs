f95931ae6408002b8152a2a5ffcd7b24d465d7cd
Tamal Mallick <tamalmallick23@gmail.com>
2025-10-10 08:54:35 +0530

    Add files via upload

---
+++ ASCII.py
@@ -0,0 +1,24 @@
+# ASCII VALUE
+
+char="a"
+print("ASCII VALUE of",char, "is", ord(char))
+
+print("********************")
+
+# Octal, Hexadecimal, Binary (8,16,2)
+
+decimal=int(input("Enter a number: "))
+print("Conversion of deicmal no.", decimal, "is: ")
+print(bin(decimal),"in binary")
+print(oct(decimal),"in octal")
+print(hex(decimal),"in hexadecimal")
+
+# Using recursion
+
+def ConvertBinary(n):
+    if n>1:
+        ConvertBinary(n//2)
+    print(n%2, end= "")
+
+num=int(input('Enter number for conversion:'))
+(ConvertBinary(num))
