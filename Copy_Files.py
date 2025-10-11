51e2d220bbf749e970d34f0cd64ec6f54f5e6407
Tamal Mallick <tamalmallick23@gmail.com>
2025-10-11 19:54:32 +0530

    Copy_Files.py

--- Copy_Files.py
+++ Copy_Files.py
@@ -1,4 +1,5 @@
 from shutil import copyfile
 copyfile("C:/Users/catas/OneDrive/Desktop/VS Code Progs/Website Link.txt", "C:/Users/catas/OneDrive/Desktop/VS Code Progs/Tamal.txt")
 
-# File contents copied from source to destination. From Website link to Tamal. 
+
+# File contents copied from source (src) to destination. From Website link to Tamal. 
