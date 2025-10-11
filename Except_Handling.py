fe27519523341c7d00b8f6314c94195f4ad631af
Tamal Mallick <tamalmallick23@gmail.com>
2025-10-11 19:50:06 +0530

    Add files via upload

---
+++ Except_Handling.py
@@ -0,0 +1,18 @@
+'''
+string=input("Enter something here: ")
+
+num=int(input("Enter a number here: "))
+
+print(string+num) # Will give errors. Sometime type error and sometimes, Value error
+'''
+
+
+string=input("Enter something here: ")
+try:
+    num=int(input("Enter a number here: "))
+    print(string+num)
+
+except ( ValueError, ValueError) as a: # Storing the values in a 
+    print(a) # Error handled. Black text. Will continue If in red, error not handled.
+
+print('Thank you.')
