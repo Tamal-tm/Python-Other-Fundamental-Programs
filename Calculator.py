43018cf7e6ef86ed29750df598ca1a7e34c8e474
Tamal Mallick <tamalmallick23@gmail.com>
2025-10-13 16:20:37 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,4 @@
 print("Mini Calculator.")
-
 num1=float(input("Enter 1st number here: "))
 num2=float(input("Enter 2nd number here: \n"))
 
@@ -17,6 +16,7 @@ elif choice ==4:
     print(num1/num2)
 else:
     print("Invalid input")
+
 
 
 
