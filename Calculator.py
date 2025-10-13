c5079fc4fbd51bf8584b4a4831d13a741a8902bb
Tamal Mallick <tamalmallick23@gmail.com>
2025-10-13 16:21:18 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -4,6 +4,7 @@ num1=float(input("Enter 1st number here: "))
 num2=float(input("Enter 2nd number here: \n"))
 
 print("Press 1 for addition\nPress 2 for Subtraction\nPress 3 for for multiplication\nPress 4 for division\n")
+
 choice=int(input("Enter your choice from 1-4: "))
 
 if choice ==1:
@@ -16,6 +17,7 @@ elif choice ==4:
     print(num1/num2)
 else:
     print("Invalid input")
+
 
 
 
