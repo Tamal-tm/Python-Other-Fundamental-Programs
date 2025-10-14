afbe098ca87ce0929d51af1ae4fed877428fabc2
Tamal Mallick <tamalmallick23@gmail.com>
2025-10-14 08:41:15 +0530

    Update Calendar.py

--- Calendar.py
+++ Calendar.py
@@ -1,10 +1,11 @@
 import calendar
 
 year=int(input("Enter year: "))
-mon_th=int(input("Enter month: "))
+mon_th=int(input("Enter month:"))
 
 calendar=calendar.month(year, mon_th) # update month
 print(calendar)
+
 
 
 
