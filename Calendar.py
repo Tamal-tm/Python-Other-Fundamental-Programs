9adc3f6a8ff5f3bb6f69c7045c7c49692b25ec6d
Tamal Mallick <tamalmallick23@gmail.com>
2025-10-14 08:40:04 +0530

    Update Calendar.py

--- Calendar.py
+++ Calendar.py
@@ -3,6 +3,7 @@ import calendar
 year=int(input("Enter year: "))
 mon_th=int(input("Enter month: "))
 
-calendar=calendar.month(year, mon_th) # month function
+calendar=calendar.month(year, mon_th) 
 print(calendar)
+
 
